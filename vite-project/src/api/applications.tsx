86b41d75934994ed535fe7d5dc0a98f1ebd5f286
SZSage <simonz@uoregon.edu>
2025-02-26 00:22:28 -0800

    feat(api): added generic API client for handling requests

---
+++ vite-project/src/api/applications.tsx
@@ -0,0 +1,7 @@
+import { apiGet } from "./api-client";
+import { Applications }  from "@/types/types";
+
+// get all applications
+export async function getApplications(): Promise<Applications[]> {
+  return apiGet<Applications[]>("applications");
+}
