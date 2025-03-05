e42378da6a0709d130b7eccda0b35a3003eec868
SZSage <simonz@uoregon.edu>
2025-03-04 18:21:06 -0800

    feat: Implement functionality for modifying and removing job applications

--- jobtracker/src/main/java/com/jobtracker/controller/JobApplicationController.java
+++ jobtracker/src/main/java/com/jobtracker/controller/JobApplicationController.java
@@ -3,10 +3,15 @@ import com.jobtracker.service.JobApplicationService;
 import com.jobtracker.model.JobApplications;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/jobApplications")
@@ -27,5 +32,15 @@ public class JobApplicationController {
     @GetMapping("/api/getApplications")
     public Iterable<JobApplications> listApplications() {
         return jobApplicationService.getJobApplications();
+    }
+
+    @PatchMapping("/api/modifyApplication/{jobId}")
+    public ResponseEntity<?> modifyApplication(@PathVariable UUID jobId ,@RequestBody JobApplications jobApplications) {
+        JobApplications updated = jobApplicationService.updateJobApplications(jobId, jobApplications);
+        return ResponseEntity.ok(updated);
+    }
+    @DeleteMapping("/api/deleteApplication/{jobId}")
+    public int deleteAplpication(@PathVariable UUID jobId) {
+        return jobApplicationService.deleteApplication(jobId);
     }
 }
