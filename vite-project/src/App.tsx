c1fdda914a347ac9bb16a2c9a93f16de47775f3c
SZSage <simonz@uoregon.edu>
2025-03-18 22:12:50 -0700

    fix: frontend updates

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,15 +1,22 @@
-import { useEffect, useState } from "react";
 import Page from "@/app/dashboard/page";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { ThemeProvider } from "@/components/theme-provider"
+import { Routes, Route, Navigate } from "react-router-dom"
+import Register from "./pages/register.tsx"
 
 export default function App() {
   return (
-    <div className="font-roboto min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900">
+    <div className="font-roboto min-h-screen bg-gradient-to-br from-gray-800 via-gray-900 to-gray-900">
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <SidebarProvider>
-          <Page />
-        </SidebarProvider>
+        <Routes>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/dashboard" element={
+            <SidebarProvider>
+              <Page />
+            </SidebarProvider>
+            }/>
+          <Route path="/" element={<Navigate to="/register" />} />
+        </Routes>
       </ThemeProvider>
     </div>
   );
