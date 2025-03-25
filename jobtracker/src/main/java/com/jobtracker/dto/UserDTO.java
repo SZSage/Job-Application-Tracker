06df51687ba5c7c949737e4347c423a62da7fae6
SZSage <simonz@uoregon.edu>
2025-03-24 19:31:10 -0700

    feat: Add data transfer object and data access object for users

--- jobtracker/src/main/java/com/jobtracker/dto/UserDTO.java
+++ jobtracker/src/main/java/com/jobtracker/dto/UserDTO.java
@@ -1,53 +1,53 @@
 package com.jobtracker.dto;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class UserDTO {
-    private UUID userId;
-    private String email;
-    private LocalDateTime createdAt = LocalDateTime.now();
-    private String role;
+  private UUID userId;
+  private String email;
+  private LocalDateTime createdAt = LocalDateTime.now();
+  private String role;
 
-    public UserDTO() {}
+  public UserDTO() {}
 
-    // constructor
-    public UserDTO(UUID userId, String email, LocalDateTime createdAt, String role) {
-        this.userId = userId;
-        this.email = email;
-        this.createdAt = createdAt;
-        this.role = role;
-    }
+  // constructor
+  public UserDTO(UUID userId, String email, LocalDateTime createdAt, String role) {
+    this.userId = userId;
+    this.email = email;
+    this.createdAt = createdAt;
+    this.role = role;
+  }
 
-    public UUID getUserId() {
-        return userId;
-    }
+  public UUID getUserId() {
+    return userId;
+  }
 
-    public String getUserEmail() {
-        return email;
-    }
+  public String getUserEmail() {
+    return email;
+  }
 
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
 
-    public String getRole() {
-        return role;
-    }
+  public String getRole() {
+    return role;
+  }
 
-    public void setUserId(UUID userId) {
-        this.userId = userId;
-    }
+  public void setUserId(UUID userId) {
+    this.userId = userId;
+  }
 
-    public void setUserEmail(String email) {
-        this.email = email;
-    }
+  public void setUserEmail(String email) {
+    this.email = email;
+  }
 
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
 
-    public void setRole(String role) {
-        this.role = role;
-    }
-
+  public void setRole(String role) {
+    this.role = role;
+  }
 }
