4dde9a4e7492d0f35e046ed414cddca1e6bb4e4d
SZSage <simonz@uoregon.edu>
2025-03-26 16:57:11 -0700

    chore: Add logging configuration and minor change to controller files

--- jobtracker/src/main/java/com/jobtracker/controller/AuthController.java
+++ jobtracker/src/main/java/com/jobtracker/controller/AuthController.java
@@ -1,23 +1,16 @@
 package com.jobtracker.controller;
 
-import jakarta.validation.Valid;
-
 import com.jobtracker.dto.UserLoginDTO;
-import com.jobtracker.dto.UserRegistrationDTO;
 import com.jobtracker.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping(path="/api/auth")
+@RestController
+@RequestMapping(path="/api/auths")
 public class AuthController {
   private final UserService userService;
 
@@ -26,29 +19,8 @@ public class AuthController {
     this.userService = userService;
   }
 
-  // #NOTE: This implementation will be used for connecting to frontend
+  /*
   @PostMapping("/registers")
-  public ResponseEntity<?> userRegister(@Valid @ModelAttribute("userRegistration") UserRegistrationDTO userRegisterDTO) {
-    try {
-      String result = userService.addUser(
-        userRegisterDTO.getEmail(),
-        userRegisterDTO.getPassword());
-      return ResponseEntity.ok(result);
-    } catch (Exception e) {
-      return ResponseEntity.badRequest().body(e.getMessage());
-    }
-  }
-
-  // #TODO: Login
-
-  // #NOTE: This current implementation returns the register.html page for testing
-  @GetMapping("/register")
-  public String showRegisterPage(Model model) {
-    model.addAttribute("userRegistration", new UserRegistrationDTO());
-    return "register";
-  }
-
-  @PostMapping("/register")
   public ResponseEntity<?> postUserRegister(@Valid @RequestBody UserRegistrationDTO userRegistrationDTO, Model model) {
     try {
       String result = userService.addUser(
@@ -61,7 +33,8 @@ public class AuthController {
     }
   }
 
-  @GetMapping("/login")
+  */
+  @PostMapping("/login")
   public String showLoginPage(Model model) {
     model.addAttribute("userLogin", new UserLoginDTO());
     return "login";
--- jobtracker/src/main/java/com/jobtracker/controller/LoginController.java
+++ jobtracker/src/main/java/com/jobtracker/controller/LoginController.java
@@ -10,9 +10,4 @@ public class LoginController {
   String login() {
     return "login";
   }
-
-  @GetMapping("/register")
-  String register() {
-    return "register";
-  }
 }
