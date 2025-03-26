f23f2d1537e5e2a786c0a1229c1416be641a9849
SZSage <simonz@uoregon.edu>
2025-03-26 16:56:05 -0700

    test: Add test for user registration

--- jobtracker/src/test/java/com/jobtracker/unittest/UserControllerTest.java
+++ jobtracker/src/test/java/com/jobtracker/unittest/UserControllerTest.java
@@ -1,14 +1,17 @@
 package com.jobtracker.unittest;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.Arrays;
+import java.util.UUID;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jobtracker.controller.UserController;
+import com.jobtracker.dto.UserRegistrationDTO;
 import com.jobtracker.model.User;
 import com.jobtracker.service.UserService;
 
@@ -20,40 +23,40 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 @WebMvcTest(UserController.class)
 @AutoConfigureMockMvc(addFilters = false)
 class UserControllerTest {
-  @Autowired
-  private MockMvc mockMvc;
+  @Autowired private MockMvc mockMvc;
 
-  @MockBean
-  private UserService userService;
+  @MockBean private UserService userService;
 
-  @Autowired
-  private ObjectMapper objectMapper; // For JSON conversion
+  @Autowired private ObjectMapper objectMapper; // For JSON conversion
 
   @Test
   @DisplayName("Should register user successfully")
   void testAddUser() throws Exception {
-    User testUser = new User("testAddUser@email.com", "password234", "USER");
-    when(userService.addUser("testAddUser@email.com", "password234")).thenReturn("User saved");
+
+    // Create registration DTO
+    UserRegistrationDTO userRegistrationDto = new UserRegistrationDTO();
+    userRegistrationDto.setEmail("testAddUser@email.com");
+    userRegistrationDto.setPassword("password123");
+    // Create expected return User
+    User createdUser = new User("testAddUser@email.com", "password234", "USER");
+    createdUser.setUserId(UUID.randomUUID());
+    createdUser.setEmail("testAddUser@email.com");
+    createdUser.setRole("USER");
+    when(userService.addUser(any(UserRegistrationDTO.class))).thenReturn(createdUser);
 
     // HTTP request building
-    mockMvc.perform(post("/api/auth/register")
-      .contentType(MediaType.APPLICATION_JSON) // Sets content type header
-      .content(objectMapper.writeValueAsString(testUser))) // Sets request body
-      .andExpect(status().isOk());
+    mockMvc
+        .perform(
+            post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON) // Sets content type header
+                .content(objectMapper.writeValueAsString(userRegistrationDto))) // Sets request body
+        .andExpect(status().isCreated()).andDo(print())
+        .andExpect(jsonPath("$.email").value("testAddUser@email.com"))
+        .andExpect(jsonPath("$.userId").isNotEmpty());
   }
 
-  @Test
-  void shouldGetAllUsers() throws Exception {
-    Iterable<User> users = Arrays.asList(new User("test@email.com", "password123", "USER"));
-    System.out.println("Users:" + users);
-    when(userService.getAllUsers()).thenReturn(users);
-
-    mockMvc.perform(get("/api/auth/all")).andDo(MockMvcResultHandlers.print())
-           .andExpect(status().isOk());
-  }
 }
