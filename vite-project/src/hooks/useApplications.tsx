4a598b08926ed51ef2687bb34642bbb26834693c
Simon Zhao <102438457+SZSage@users.noreply.github.com>
2025-07-22 17:12:16 -0700

    refactor: Remove unnecessary object creation and simplify logic for custom hook

--- vite-project/src/hooks/useApplications.tsx
+++ vite-project/src/hooks/useApplications.tsx
@@ -1,11 +1,6 @@
 import { useState, useEffect } from "react";
 import { getApplications } from "@/api/applications-api";
 
-interface jobInfo {
-  checked: boolean;
-  status: number;
-}
-
 export interface Applications {
   jobId: string;
   jobTitle: string;
@@ -31,27 +26,15 @@ export type ApplicationStatus =
 // Custom hook for fetching applications, job data, and calculating job count
 export function useApplications() {
   const [applications, setApplications] = useState<Applications[]>([]);
-  const [jobData, setJobData] = useState<Record<string, jobInfo>>({});
   const [totalCount, setTotalCount] = useState<number>(0);
 
   useEffect(() => {
     getApplications().then((applications) => {
-      // Creating new object to store job data
-      const jobs: Record<string, jobInfo> = {};
-      applications.forEach((job) => {
-        jobs[job.jobId] = {
-          checked: false,
-          status: parseInt(job.statusId),
-        };
-      });
-
-      setJobData(jobs);
       setApplications(applications);
       setTotalCount(applications.length);
-      console.log("JOBS: " + JSON.stringify(jobs));
     })
-    .catch((error) => console.error("Failed to fetch data: " + error));
+    .catch((error) => console.error("Failed to fetch data: ", error));
   }, []);
 
-  return { applications, jobData, totalCount };
+  return { applications, totalCount };
 }
