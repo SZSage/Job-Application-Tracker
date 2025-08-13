4f56679f18a8b796ba0981eef1b12b3ce920e1fc
Simon Zhao <102438457+SZSage@users.noreply.github.com>
2025-08-13 15:11:13 -0700

    refactor: Utilize react-query mutation for adding applications

--- vite-project/src/features/applications/addApplication.tsx
+++ vite-project/src/features/applications/addApplication.tsx
@@ -1,4 +1,5 @@
-import { addApplication } from "@/api/applications-api";
+import { useState } from "react";
+import { useAddApplications } from "@/hooks/useApplications";
 import { Plus } from "lucide-react";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
@@ -13,96 +14,87 @@ import {
 } from "@/components/ui/dialog";
 
 export function AddApplication() {
-  const handleSubmit = (event: any) => {
-    console.log("Form submitted: ", event);
+  // Utilize useJobMutations
+  const { addJobMutation } = useAddApplications();
+  const [ isOpen, setIsOpen] = useState<boolean>(false);
+    // Construct delete payload and calls API
+  const handleAddSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-
-    const formData = new FormData(event.target);
-
-    const applicationData = {
-      jobId: formData.get("job-id"),
-      jobTitle: formData.get("job-title"),
-      companyName: formData.get("company-name"),
-      location: formData.get("location"),
-      salary: Number(formData.get("salary")),
-      status: Number(formData.get("status")),
-      userId: sessionStorage.getItem("userId"),
-    };
-
-    console.log("Applicaiton Data:", applicationData);
-    console.log("Form data:", Object.fromEntries(formData));
-
-    //send API request with form data
-    addApplication(applicationData)
-      .then((applicationData) => {
-        console.log("Application added successfully!", applicationData);
-      })
-      .catch((error) => {
-        console.log("Error adding application", error);
+    const formData = new FormData(event.target as HTMLFormElement);
+    console.log("Form data: ", formData);
+    try {
+      await addJobMutation({
+        jobTitle: formData.get("jobTitle") as string,
+        companyName: formData.get("companyName") as string,
+        location: formData.get("location") as string,
+        salary: Number(formData.get("salary")),
+        statusId: Number(formData.get("statusId")),
+        userId: sessionStorage.getItem("userId") as string,
       });
+      setIsOpen(false)
+    } catch (error) {
+      console.error("Failed to add application:", error);
+    }
   };
 
   return (
-    <Dialog>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger asChild>
         <Button className="font-bold">
           <Plus />
-          Add Application
         </Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w[450px]">
+      <DialogContent className="sm:max-w-[450px]">
         <DialogHeader>
-          <DialogTitle>Add New Application</DialogTitle>
+          <DialogTitle>Add application</DialogTitle>
         </DialogHeader>
-
-        <form onSubmit={handleSubmit}>
+        {/* Add application form */}
+        <form onSubmit={handleAddSubmit}>
           <div className="grid gap-4 py-4">
+
             <div className="grid grid-cols-1 gap-4 items-center">
-              <Label htmlFor="job-title" className="text-right">
+              <Label htmlFor="jobTitle" className="text-right">
                 Job Title
               </Label>
-              <Input name="job-title" className="col-span-3" />
+              <Input id="jobTitle" name="jobTitle" className="col-span-3" />
             </div>
 
             <div className="grid grid-cols-1 items-center gap-4">
-              <Label htmlFor="company-name" className="text-center">
+              <Label htmlFor="companyName" className="text-center">
                 Company Name
               </Label>
-              <Input name="company-name" className="col-span-3" />
+              <Input id="companyName" name="companyName" className="col-span-3" />
             </div>
 
             <div className="grid grid-cols-1 items-center gap-4">
               <Label htmlFor="location" className="text-center">
                 Location
               </Label>
-              <Input name="location" className="col-span-3" />
+              <Input id="location" name="location" className="col-span-3" />
             </div>
 
             <div className="grid grid-cols-1 items-center gap-4">
               <Label htmlFor="salary" className="text-center">
                 Salary
               </Label>
-              <Input name="salary" className="col-span-3" />
+              <Input id="salary" name="salary" className="col-span-3" />
             </div>
 
             <div className="grid grid-cols-1 items-center gap-4">
-              <Label htmlFor="status" className="text-center">
+              <Label htmlFor="statusId" className="text-center">
                 Status
               </Label>
-              <Input name="status" className="col-span-3" />
+              <Input id="statusId" name="statusId" className="col-span-3" />
             </div>
+
           </div>
           <DialogFooter>
-            <Button
-              type="submit"
-              className="font-bold rounded-lg"
-            >
+            <Button type="submit" className="font-bold rounded-lg">
               Save changes
             </Button>
           </DialogFooter>
         </form>
       </DialogContent>
     </Dialog>
-  )
+  );
 }
-
